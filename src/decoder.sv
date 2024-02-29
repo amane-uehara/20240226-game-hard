884b69d70b69ec241b3bf7f3e52d9ac7dc5db9b1
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-02-29 23:34:04 +0900

    bugfix

--- src/decoder.sv
+++ src/decoder.sv
@@ -1,7 +1,7 @@
 `include "lib_cpu.sv"
 
 module decoder import lib_cpu :: *; (
-  output DECODE       de
+  output DECODE       de,
   input  REGISTERS    regs,
   input  logic [31:0] instruction,
   input  logic        w_busy,
@@ -9,16 +9,16 @@ module decoder import lib_cpu :: *; (
   input  logic [31:0] r_data
 );
   assign de = '0;
-  assign de.calcr = instruction[3:0]
-  assign de.opt   = instruction[7:4]
-  assign de.rd    = instruction[11:8]
-  assign de.rs1   = instruction[15:12]
-  assign de.rs2   = instruction[19:16]
+  assign de.calcr = instruction[3:0];
+  assign de.opt   = instruction[7:4];
+  assign de.rd    = instruction[11:8];
+  assign de.rs1   = instruction[15:12];
+  assign de.rs2   = instruction[19:16];
 
   logic s;
   assign s = instruction[31];
-  assign de.imm12 = {{20{s}, instruction[31:20]};
-  assign de.imm16 = {{16{s}, instruction[31:16]};
+  assign de.imm12 = {{20{s}}, instruction[31:20]};
+  assign de.imm16 = {{16{s}}, instruction[31:16]};
 
   assign de.x_rd  = regs.x[de.rd];
   assign de.x_rs1 = regs.x[de.rs1];
