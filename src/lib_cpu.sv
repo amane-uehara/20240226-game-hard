284fb1ac0023237e068617f5fd57cf6b2443e66c
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-03-01 00:04:39 +0900

    fix lib_cpu

--- src/lib_cpu.sv
+++ src/lib_cpu.sv
@@ -12,7 +12,7 @@ package lib_cpu;
   typedef struct packed {
     logic [ 3:0] opcode;
     logic [ 3:0] opt;
-    logic [ 3:0] rsd;
+    logic [ 3:0] rd;
     logic [ 3:0] rs1;
     logic [ 3:0] rs2;
     logic [31:0] imm12;
@@ -32,9 +32,9 @@ package lib_cpu;
 
   typedef struct packed {
     logic [31:0] pc;
-    logic        w_req
+    logic        w_req;
     logic [31:0] w_data;
-    logic        ack
+    logic        ack;
     logic [ 3:0] rd;
     logic [31:0] x_rd;
     logic [ 7:0] addr_4byte;
