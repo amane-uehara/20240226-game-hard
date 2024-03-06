d97c14c8a67e12496315b792c2bf466971156030
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-03-06 17:45:33 +0900

    intel

--- board/de10_nano/add_file.tcl
+++ board/de10_nano/add_file.tcl
@@ -1,12 +1,17 @@
 package require ::quartus::project
 package require fileutil
 
+foreach file [fileutil::findByPattern "." top.sv] {
+  puts $file
+  set_global_assignment -name SYSTEMVERILOG_FILE $file
+}
+
 foreach file [fileutil::findByPattern "../../src" *.sv] {
   puts $file
   set_global_assignment -name SYSTEMVERILOG_FILE $file
 }
 
-foreach file [fileutil::findByPattern "../../mem" *] {
+foreach file [fileutil::findByPattern "../../mem" *.mem] {
   puts $file
   set_global_assignment -name MIF_FILE $file
 }
