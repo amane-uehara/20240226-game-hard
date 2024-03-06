c3dc3034f58687808198e9a53e062bd376ca9f39
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-03-06 20:41:58 +0900

    parameter

--- src/mother_board.sv
+++ src/mother_board.sv
@@ -1,11 +1,11 @@
-module mother_board (
+module mother_board #(parameter WAIT, FILENAME) (
   input  logic clk, reset,
   input  logic uart_rx,
   output logic uart_tx
 );
   logic [7:0] uart_r_data;
   logic uart_update;
-  receiver receiver (
+  receiver #(.WAIT(WAIT)) receiver (
     .clk, .reset,
     .uart_rx,
     .update(uart_update),
@@ -14,7 +14,7 @@ module mother_board (
 
   logic [7:0] uart_w_data;
   logic uart_w_req, uart_busy;
-  transmitter transmitter (
+  transmitter #(.WAIT(WAIT)) transmitter (
     .clk, .reset,
     .uart_tx,
     .send_req(uart_w_req),
@@ -24,11 +24,20 @@ module mother_board (
 
   logic irr;
   logic ack;
-  uart_intr uart_intr (.clk, .reset, .uart_update, .ack, .irr);
+  uart_intr uart_intr (
+    .clk, .reset,
+    .uart_update,
+    .ack,
+    .irr
+  );
 
   logic [10:0] rom_addr;
   logic [31:0] rom_data;
-  rom rom (.clk, .addr(rom_addr), .data(rom_data));
+  rom #(.FILENAME(FILENAME)) rom (
+    .clk,
+    .addr(rom_addr),
+    .data(rom_data)
+  );
 
   cpu cpu (
     .clk, .reset,
