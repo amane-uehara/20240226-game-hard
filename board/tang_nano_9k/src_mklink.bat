cbe5727fc5e71e6fbf8ec0cb01551d294d994818
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-03-18 23:55:25 +0900

    mklink

---
+++ board/tang_nano_9k/src_mklink.bat
@@ -0,0 +1,14 @@
+@echo off
+set target_dir=%cd%\20240226-game-hard\src
+
+set source_dir=%cd%\..\..\src
+for %%i in ("%source_dir%\*.sv")  do (mklink "%target_dir%\%%~nxi" "%%i")
+
+set source_dir=%cd%\..\..\mem
+for %%i in ("%source_dir%\*.mem") do (mklink "%target_dir%\%%~nxi" "%%i")
+
+set source_dir=%cd%
+for %%i in ("%source_dir%\*.sv")  do (mklink "%target_dir%\%%~nxi" "%%i")
+
+set source_dir=%cd%
+for %%i in ("%source_dir%\*.cst") do (mklink "%target_dir%\%%~nxi" "%%i")
