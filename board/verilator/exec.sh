a9cf0c4f8bd06ee025eb99fd8c6cb0e4b303c0cc
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-04-16 03:12:22 +0900

    exec.sh

---
+++ board/verilator/exec.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+VERILOG_FILE=$(basename $1)
+VERILOG_WITHOUT_EXT=${VERILOG_FILE%.*}
+
+cd ${SCRIPT_DIR}/tb \
+  && pwd \
+  && verilator \
+      --timing \
+      --binary -j 0 \
+      -y ${SCRIPT_DIR}/../../src/ \
+      -y ${SCRIPT_DIR}/../../mem/ \
+      ${VERILOG_FILE} \
+  && ./obj_dir/V${VERILOG_WITHOUT_EXT} \
+  && cd ${SCRIPT_DIR}
+
