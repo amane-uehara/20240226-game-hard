3cd77db80b224f49a5ae0b48f119d38bd2121b26
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-04-17 00:04:04 +0900

    test_top

--- board/verilator/tb/tb2.sv
+++ board/verilator/tb/tb2.sv
@@ -2,9 +2,10 @@
 
 module tb2 ();
   import test_package :: *;
+
   logic clk, reset, uart_rx, uart_tx;
   test_clock test_clock(clk);
-  assign uart_rx = 1'b1;
+  assign uart_rx = 1'b1; // no signal
 
   mother_board #(.WAIT(8), .FILENAME("")) mother_board(.*);
 
@@ -16,7 +17,7 @@ module tb2 ();
     fn_expected_actual_check(`__FILE__, line_number, expected, actual);
   endfunction
 
-  task automatic task_result_wait(input int delay_cycle);
+  task automatic task_reset_wait(input int delay_cycle);
     reset = 1'b1;
     #RESET_PERIOD;
     reset = 1'b0;
@@ -46,9 +47,10 @@ module tb2 ();
 
     i = 0; //                       imm  rs2 rs1 rd  opt opcode
     mother_board.rom.mem[i++] = 32'h003___0___0___2___0___0; // addi ---- x[2] = x[0] + 3
-    mother_board.rom.mem[i++] = 32'h001___0___2___3___0___0; // addi ---- x[3] = x[2] + 1
+    mother_board.rom.mem[i++] = 32'h004___0___2___3___0___0; // addi ---- x[3] = x[2] + 4
     mother_board.rom.mem[i++] = 32'h000___3___2___0___0___5; // sw   ---- mem[x[2]] = x[3]
     mother_board.rom.mem[i++] = 32'h000___0___0___0___0___A; // halt
     task_reset_wait(i);
     check(`__LINE__, 32'd7, mem[3]);
+  end
 endmodule
--- board/verilator/tb/test_top.sv
+++ board/verilator/tb/test_top.sv
@@ -1,24 +1,13 @@
+`include "test_package.sv"
+
 module test_top ();
-  logic clk;
-
-  localparam CLOCK_PERIOD = 10;
-  always #(CLOCK_PERIOD/2) clk <= ~clk;
-  initial clk = 1'b0;
-
-  logic [31:0] tb1_pass, tb1_fail;
-  tb1 tb1(.pass(tb1_pass), .fail(tb1_fail));
-
-  logic [31:0] tb2_pass, tb2_fail;
-  tb2 tb2(.pass(tb2_pass), .fail(tb2_fail));
-
-  logic [31:0] total_pass, total_fail;
-  assign total_pass = tb1_pass + tb2_pass;
-  assign total_fail = tb1_fail + tb2_fail;
+  import test_package :: *;
+  tb1 tb1;
+  tb2 tb2;
 
   initial begin
     #(CLOCK_PERIOD*100);
-    $display("TOTAL PASS: %d", total_pass);
-    $display("TOTAL FAIL: %d", total_fail);
+    fn_show_total_result();
     $finish;
   end
 endmodule
