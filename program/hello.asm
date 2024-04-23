d843986352cd0361b553b8c0598be5e05555bf06
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-04-24 06:01:00 +0900

    add macro

--- program/hello.asm
+++ program/hello.asm
@@ -1,15 +1,24 @@
+label_start:
+  a = 72
+  sp = sp - 4
+  mem[sp] = ra
+  ra = label_putchar
+  (pc, ra) = (ra, pc + 4)
+  ra = mem[sp]
+  sp = sp + 4
+
+  a = 69
+  sp = sp - 4
+  mem[sp] = ra
+  ra = label_putchar
+  (pc, ra) = (ra, pc + 4)
+  ra = mem[sp]
+  sp = sp + 4
+
 label_putchar:
   label_do_while_1_bgn:
     b = monitor_busy()
     c = label_do_while_1_bgn
     if (b == 0) pc = c
   monitor(a)
-  pc=ra
-
-a = 72
-b = label_putchar
-ra=pc+4, pc=b
-
-a = 69
-b = label_putchar
-ra=pc+4, pc=b
+  pc = ra
---
+++ program/substitute.py
@@ -0,0 +1,56 @@
+import re
+import sys
+
+LABEL = "(?P<label>(label[_0-9a-z]*))"
+
+def substitute_call(line):
+  ret = []
+  m = re.match(f"^call\({LABEL}\)$", line)
+  if m:
+    label = m.groupdict()["label"]
+    ret.append("sp = sp - 1")
+    ret.append("mem[sp] = ra")
+    ret.append(f"ra = {label}")
+    ret.append("(pc, ra) = (ra, pc + 1)")
+    ret.append("ra = mem[sp]")
+    ret.append("sp = sp + 1")
+  return ret
+
+def substitute_push(line):
+  ret = []
+  m = re.match(f"^push\((?P<regs>.*)\)$", line)
+  if m:
+    for reg in m.groupdict()["regs"].split(","):
+      ret.append("sp = sp - 1")
+      ret.append(f"mem[sp] = {reg}")
+  return ret
+
+def substitute_pop(line):
+  ret = []
+  m = re.match(f"^push\((?P<regs>.*)\)$", line)
+  if m:
+    for reg in m.groupdict()["regs"].split(","):
+      ret.append(f"{reg} = mem[sp]")
+      ret.append("sp = sp + 1")
+  return ret
+
+def main():
+  filename = sys.argv[1]
+
+  ret = []
+  with open(filename) as f:
+    for line_raw in f:
+      line = line_raw.replace(" ","")
+      substitute = []
+      substitute += substitute_call(line)
+      substitute += substitute_push(line)
+      substitute += substitute_pop(line)
+
+      if substitute:
+        ret += substitute
+      else:
+        ret.append(line_raw.strip())
+
+  print('\n'.join(ret))
+
+main()
