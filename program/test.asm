d3c37deae0143b4dce40f2f269d7ca6473e1c8d2
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-04-24 19:40:46 +0900

    test.asm

--- program/test.asm
+++ program/test.asm
@@ -1,66 +1,19 @@
-a = 57
-d = e + 3
-zero = zero + 3
-
-a = b + c
-a = b
-
-b = label_1
-a = pc + 4, pc = b
-label_1:
-
-c = label_2
-pc = c
-label_2:
-
-d = label_3
-if (e == 0) pc = d
-label_3:
-
-d = label_4
-if (f != 0) pc = d
-label_4:
-
-d = label_5
-if (g >= 0) pc = d
-label_5:
-
-d = label_6
-if (h <  0) pc = d
-label_6:
-
-h = mem[i]
-mem[f] = g
-
-a = 0
-b = io(a)
-
+label_start:
 a = 1
-b = io(a)
+for (b=2; c<0; d=e+1) {
+  a = 2
+  if (f == 0) {
+    a = 3
+    call(label_func)
+  }
+  a = 4
+  if (g > 0) {
+    a = 5
+  }
+  a = 6
+}
+a = 7
 
-io(zero) = c
-
-a = 0
-b = 1
-intr(a) = b
-
-a = 1
-intr(a) = b
-
-a = 2
-b = label_trap
-intr(a) = b
-
-a = 0
-halt()
-
-label_trap:
-b = 1
-intr(zero) = b
-a = 1
-iret()
-a = 2
-
-label_not_reach:
-a = 3
-halt()
+label_func:
+  push(a,b,c)
+  pop(a,b,c)
