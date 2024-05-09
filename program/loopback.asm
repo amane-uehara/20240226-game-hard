e8d631efc3f64f24bd49eef29b63747b0b0d9763
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-05-09 21:07:25 +0900

    loopbackを改良

--- program/loopback.asm
+++ program/loopback.asm
@@ -1,19 +1,14 @@
-label_start:
-  intr_en(zero)
-  a = label_trap
-  intr_trap(a)
-  a = 1
-  intr_en(a)
-
-label_halt:
-  halt()
-  a = label_halt
-  pc = a
+label_main:
+a = label_trap
+intr_trap(a)
+b = 1
+intr_en(b)
+halt()
 
 label_trap:
-  a = keyboard()
-  b = 1
-  intr_ack(b)
-  a = a + 2
-  monitor(a)
-  iret()
+a = keyboard()
+b = 1
+intr_ack(b)
+a = a + 2
+monitor(a)
+iret()
