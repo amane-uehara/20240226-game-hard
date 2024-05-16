937da484faece4aeaa385136a9afd0782e1e0bce
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-05-17 06:01:00 +0900

    update argument

--- program/substitute.py
+++ program/substitute.py
@@ -1,34 +1,6 @@
 import re
 import sys
 
-def substitute_call(line):
-  ret = []
-  fn_name = ""
-  DST = "(?P<dst>([a-z]|zero|sp|ra|rv|tptr|tcmp))"
-  FN_NAME = "(?P<fn_name>(fn_[a-z0-9_]*))"
-  ARGUMENT  = "(?P<argument>(..*))"
-
-  m = re.match(f"^({DST}=)?{FN_NAME}\({ARGUMENT}?\)$", line)
-
-  if m:
-    if m.groupdict()["argument"]:
-      argument = m.groupdict()["argument"]
-      ret.append(f"rv = {argument}")
-
-    fn_name = m.groupdict()["fn_name"]
-    ret.append("sp = sp - 1")
-    ret.append("mem[sp] = ra")
-    ret.append(f"ra = label_{fn_name}")
-    ret.append("(pc, ra) = (ra, pc + 1)")
-    ret.append("ra = mem[sp]")
-    ret.append("sp = sp + 1")
-
-    if m.groupdict()["dst"]:
-      dst = m.groupdict()["dst"]
-      ret.append(f"{dst} = rv")
-
-  return ret
-
 def substitute_comp(line):
   ret = []
   SRC1 = "(?P<src1>([^=!<>]*))"
@@ -62,6 +34,37 @@ def substitute_pop(line):
     for reg in m.groupdict()["regs"].split(","):
       ret.append(f"{reg} = mem[sp]")
       ret.append("sp = sp + 1")
+  return ret
+
+def substitute_call(line):
+  ret = []
+  fn_name = ""
+  DST = "(?P<dst>([a-z]|zero|sp|ra|rv|tptr|tcmp))"
+  FN_NAME = "(?P<fn_name>(fn_[a-z0-9_]*))"
+  ARGUMENT  = "(?P<argument>(..*))"
+
+  m = re.match(f"^({DST}=)?{FN_NAME}\({ARGUMENT}?\)$", line)
+
+  if m:
+    push_list = ["ra"]
+    if m.groupdict()["argument"]:
+      push_list += m.groupdict()["argument"].split(",")
+
+    for reg_or_imm in reversed(push_list):
+      ret.append("sp = sp - 1")
+      ret.append(f"tcmp = {reg_or_imm}")
+      ret.append("mem[sp] = tcmp")
+
+    fn_name = m.groupdict()["fn_name"]
+    ret.append(f"ra = label_{fn_name}")
+    ret.append("(pc, ra) = (ra, pc + 1)")
+    ret.append("ra = mem[sp]")
+    ret.append(f"sp = sp + {len(push_list)}")
+
+    if m.groupdict()["dst"]:
+      dst = m.groupdict()["dst"]
+      ret.append(f"{dst} = rv")
+
   return ret
 
 def main():
