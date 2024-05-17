6b089ba6804484b0fb159c2877fb021402f29345
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-05-18 00:44:03 +0900

    separate compile

--- program/build.sh
+++ program/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-python3 control.py 2048.myc > a.txt
-python3 substitute.py a.txt > b.txt
-python3 assembler.py b.txt > c.txt
-cp c.txt ../mem/rom.mem
-diff 2048.txt c.txt | head -n 10
+cat stdio.myc 2048.myc > a.txt
+python3 control.py a.txt > b.txt
+python3 substitute.py b.txt > c.txt
+python3 assembler.py c.txt > d.txt
+cp d.txt ../mem/rom.mem
+diff 2048.txt d.txt | head -n 10
