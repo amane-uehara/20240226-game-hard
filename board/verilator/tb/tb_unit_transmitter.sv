8630b56439ef20aa3576015e3d9eaa7806f38d63
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-06-17 03:38:18 +0900

    transmitter test

--- board/verilator/tb/tb_unit_transmitter.sv
+++ board/verilator/tb/tb_unit_transmitter.sv
@@ -9,28 +9,8 @@ module tb_unit_transmitter();
 
   test_clock test_clock(clk);
 
-  // uart task
-  task automatic task_check_uart_1bit(input int line_number, input logic e, input logic a);
-    fn_expected_actual_check(`__FILE__, line_number, {31'd0, e}, {31'd0, a});
-    fn_expected_actual_check(`__FILE__, line_number, 32'd1, {31'd0, busy});
-    #(WAIT*CLOCK_PERIOD-1);
-    fn_expected_actual_check(`__FILE__, line_number, {31'd0, e}, {31'd0, a});
-    fn_expected_actual_check(`__FILE__, line_number, 32'd1, {31'd0, busy});
-    #1;
-  endtask
-
-  task automatic task_uart_tx(input logic [7:0] tmp_data);
-    task_check_uart_1bit(`__LINE__, 1'b0, uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[0], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[1], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[2], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[3], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[4], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[5], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[6], uart_tx);
-    task_check_uart_1bit(`__LINE__, tmp_data[7], uart_tx);
-    task_check_uart_1bit(`__LINE__, 1'b1, uart_tx);
-  endtask
+  logic [7:0] const_data;
+  assign const_data = 8'h5A;
 
   initial begin
     reset = 1'b1;
@@ -39,7 +19,7 @@ module tb_unit_transmitter();
     #RESET_PERIOD;
 
     send_req = 1'b1;
-    data = 8'h5A;
+    data = const_data;
     @(posedge clk);
     @(posedge clk);
     send_req = 1'b0;
@@ -47,18 +27,40 @@ module tb_unit_transmitter();
   end
 
   initial begin
-    @(negedge reset);
-    #RESET_PERIOD;
-    `check32(32'd1, {31'd0, uart_tx});
-    `check32(32'd0, {31'd0, busy});
-  end
-
-  initial begin
     @(posedge send_req);
     @(posedge clk);
+
+    `check1(1'b1, uart_tx);
+
+    `check1(1'b0, busy);
     #1;
-    task_uart_tx(8'h5A);
-    `check32(32'd1, {31'd0, uart_tx});
-    `check32(32'd0, {31'd0, busy});
+    `check1(1'b1, busy);
+
+    `check1(1'b0         , uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(1'b0         , uart_tx); #1;
+    `check1(const_data[0], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[0], uart_tx); #1;
+    `check1(const_data[1], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[1], uart_tx); #1;
+    `check1(const_data[2], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[2], uart_tx); #1;
+    `check1(const_data[3], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[3], uart_tx); #1;
+    `check1(const_data[4], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[4], uart_tx); #1;
+    `check1(const_data[5], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[5], uart_tx); #1;
+    `check1(const_data[6], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[6], uart_tx); #1;
+    `check1(const_data[7], uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(const_data[7], uart_tx); #1;
+    `check1(1'b1         , uart_tx); #(WAIT*CLOCK_PERIOD-1);
+    `check1(1'b1         , uart_tx);
+
+    `check1(1'b1, busy);
+    #1;
+    `check1(1'b0, busy);
+
+    `check1(1'b1, uart_tx);
   end
 endmodule
