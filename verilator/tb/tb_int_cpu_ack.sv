803eb4ba41cc28ac19fefb7e3dc9ef674c83e693
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-06-24 01:04:32 +0900

    counter 2bit

--- verilator/tb/tb_int_cpu_ack.sv
+++ verilator/tb/tb_int_cpu_ack.sv
@@ -9,6 +9,12 @@ module tb_int_cpu_ack ();
 
   mother_board #(.WAIT(8), .FILENAME("")) mother_board(.*);
 
+  task automatic task_reset();
+    reset = 1'b1;
+    #RESET_PERIOD;
+    reset = 1'b0;
+  endtask
+
   logic [15:0][31:0] x;
   assign x = mother_board.cpu.gr_file.x;
 
@@ -18,12 +24,10 @@ module tb_int_cpu_ack ();
     mother_board.rom.mem[j++] = 32'h001___0___0___1___0___0; // addi ---- x[1] = x[0] + 1
     mother_board.rom.mem[j++] = 32'h000___0___1___0___0___8; // w_intr -- intr[0] = x[1] // intr[0] == ack
     mother_board.rom.mem[j++] = 32'h000___0___0___0___0___A; // halt
+    task_reset();
     `check1(1'b0, mother_board.cpu.ack);
 
-    reset = 1'b1;
-    #RESET_PERIOD;
-    reset = 1'b0;
-    #(PERIOD_PER_INSTRUCT*2);
+    #(PERIOD_PER_INSTRUCT*2-2*CLOCK_PERIOD);
     `check1(1'b0,  mother_board.cpu.ack);
     #CLOCK_PERIOD;
     `check1(1'b1,  mother_board.cpu.ack);
