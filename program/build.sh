15467e37a3c68d874448383f30a7f0da60c46b12
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-06-29 00:12:20 +0900

    build

--- program/build.sh
+++ program/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-cat stdio.myc 2048.myc > a.txt
-python3 control.py a.txt > b.txt
-python3 substitute.py b.txt > c.txt
-python3 assembler.py c.txt > d.txt
-cp d.txt ../mem/rom.mem
-diff 2048.txt d.txt | head -n 10
+FILEROOT=$(echo $1 |sed -e 's/.myc//')
+cat stdio.myc ${FILEROOT}.myc > tmp_${FILEROOT}_1.txt
+python3 control.py tmp_${FILEROOT}_1.txt > tmp_${FILEROOT}_2.txt
+python3 substitute.py tmp_${FILEROOT}_2.txt > tmp_${FILEROOT}_3.txt
+python3 assembler.py tmp_${FILEROOT}_3.txt > tmp_${FILEROOT}_4.txt
+cp tmp_${FILEROOT}_4.txt ../mem/rom.mem
+#diff 2048.txt d.txt | head -n 10
