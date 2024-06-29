351a079f26f8887a02d409636cea28911864f197
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-06-29 11:17:04 +0900

    wave script

--- verilator/tb/test_wave.sv
+++ verilator/tb/test_wave.sv
@@ -1,16 +1,13 @@
-//`timescale 1ns/1ps
-`include "lib_cpu.sv"
+`include "test_package.sv"
 
 module test_wave ();
-  logic clk;
-  logic reset;
-  logic uart_tx;
-  logic uart_rx;
+  import test_package :: *;
+
+  logic clk, reset, uart_rx, uart_tx;
+  test_clock test_clock(clk);
+  assign uart_rx = 1'b1; // no signal
 
   localparam FILENAME = "../mem/rom.mem";
-  localparam WAIT = 8;
-  localparam CLOCK_PERIOD = 10;
-
   mother_board #(
     .WAIT(WAIT),
     .FILENAME(FILENAME)
@@ -21,53 +18,34 @@ module test_wave ();
     $dumpvars(0, mother_board);
   end
 
-  always #(CLOCK_PERIOD/2) clk <= ~clk;
-  initial clk = 1'b0;
+  task automatic task_uart_rx(input logic [7:0] tmp);
+    uart_rx = 1'b0;   #(WAIT*CLOCK_PERIOD); // start bit
+    uart_rx = tmp[0]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[1]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[2]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[3]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[4]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[5]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[6]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = tmp[7]; #(WAIT*CLOCK_PERIOD);
+    uart_rx = 1'b1;   #(WAIT*CLOCK_PERIOD); // stop bit
+  endtask
 
   initial begin
     reset = 1'b1;
-    #10;
+    #RESET_PERIOD;
     reset = 1'b0;
   end
 
   initial begin
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // no signal
-    #2000;
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b0; // start bit
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // r_data[0]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // r_data[1]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // r_data[2]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // r_data[3]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b0; // r_data[4]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b0; // r_data[5]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b0; // r_data[6]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b0; // r_data[7]
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // stop bit
-    #(WAIT*CLOCK_PERIOD);
-    $display("Current time = %t", $realtime);
-    uart_rx = 1'b1; // no signal
-    #2000;
-    $display("Current time = %t", $realtime);
+    for(;;) begin
+      $display("Current time = %t", $realtime);
+      #(WAIT*CLOCK_PERIOD);
+    end
+  end
+
+  initial begin
+    #(WAIT*CLOCK_PERIOD*50);
     $finish();
   end
 endmodule
