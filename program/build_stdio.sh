324609d00abb775543b866646baea5e2d9cafda1
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-06-29 10:52:13 +0900

    build script

--- program/build_stdio.sh
+++ program/build_stdio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FILEROOT=$(echo $1 |sed -e 's/.myc//')
-cat ${FILEROOT}.myc > tmp_${FILEROOT}_1.txt
+cat stdio.myc ${FILEROOT}.myc > tmp_${FILEROOT}_1.txt
 python3 control.py tmp_${FILEROOT}_1.txt > tmp_${FILEROOT}_2.txt
 python3 substitute.py tmp_${FILEROOT}_2.txt > tmp_${FILEROOT}_3.txt
 python3 assembler.py tmp_${FILEROOT}_3.txt > tmp_${FILEROOT}_4.txt
