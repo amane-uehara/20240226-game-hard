75e89d3de55d55c869247ac396cf09f4842edb23
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-07-12 22:35:16 +0900

    build

--- program/build_stdio.sh
+++ program/build_stdio.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 FILEROOT=$(echo $1 |sed -e 's/.myc//')
-cat stdio.myc ${FILEROOT}.myc > tmp_${FILEROOT}_1.txt
-python3 control.py tmp_${FILEROOT}_1.txt > tmp_${FILEROOT}_2.txt
-python3 substitute.py tmp_${FILEROOT}_2.txt > tmp_${FILEROOT}_3.txt
-python3 linker.py tmp_${FILEROOT}_3.txt > tmp_${FILEROOT}_4.txt
-python3 assembler.py tmp_${FILEROOT}_4.txt > tmp_${FILEROOT}_5.txt
-cp tmp_${FILEROOT}_5.txt ../mem/rom.mem
+cat stdio.myc ${FILEROOT}.myc > ./tmp/${FILEROOT}_1.txt
+python3 control.py ./tmp/${FILEROOT}_1.txt > ./tmp/${FILEROOT}_2.txt
+python3 substitute.py ./tmp/${FILEROOT}_2.txt > ./tmp/${FILEROOT}_3.txt
+python3 linker.py ./tmp/${FILEROOT}_3.txt > ./tmp/${FILEROOT}_4.txt
+python3 assembler.py ./tmp/${FILEROOT}_4.txt > ./tmp/${FILEROOT}_5.txt
+cp ./tmp/${FILEROOT}_5.txt ../mem/rom.mem
 #diff 2048.txt d.txt | head -n 10
